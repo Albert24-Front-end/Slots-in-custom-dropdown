84317fbaf62793aab78ae4e13be8dd4d41cc9a19
Albert24-Front-end <alievalbert2000@mail.ru>
2026-04-26 22:32:46 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import MyDetails from './components/MyDetails.vue';
+</script>
+
+<template>
+    <h1>Learning slots in Vue 3</h1>
+    <MyDetails title="Custom title">
+        <p>Details content</p>
+    </MyDetails>
+</template>
---
+++ src/components/MyDetails.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const {title = 'Spoiler'} = defineProps<{
+    title?: string,
+}>()
+</script>
+
+<template>
+    <div class="details">
+        <div class="title">{{ title }}</div>
+        <div class="content"><slot></slot></div>
+    </div>
+</template>
+
+<style scoped>
+.details {
+    border: 1px solid black;
+}
+
+.title {
+    background-color: lightgray;
+    cursor: pointer;
+    padding: 4px;
+}
+
+.content {
+    padding: 8px;
+}
+</style>
